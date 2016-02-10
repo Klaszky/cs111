25edcc9729886eb67b5706eca4d972c8f006acd8
Caius <jfklaszky@gmail.com>
2016-02-09 20:04:58 -0500

    minor changes

--- Party.java
+++ Party.java
@@ -2,7 +2,7 @@
 public class Party{
 
  public static void main(String[] args){
-
+  //welcome message
   System.out.println("java Party\n");
 
   double peopleAtParty, numSlicesPerPerson, numSodasPerPerson;
@@ -12,27 +12,36 @@ public class Party{
 
   double totalCost;
 
-  System.out.print("Enter number of people attening the party: ");
+  //getting part info from user
+  System.out.print("Enter number of people attending the party: ");
   peopleAtParty = IO.readDouble();
+
   System.out.print("\nHow many slices of pizza will each person eat: ");
   numSlicesPerPerson = IO.readDouble();
+
   System.out.print("\nHow many cans of soda will each person drink: ");
   numSodasPerPerson = IO.readDouble();
+
   System.out.print("\nHow much will each pizza pie cost: ");
   costOfPizza = IO.readDouble();
+
   System.out.print("\nHow many slices does each pie contain: ");
   numSlicesPerPizza = IO.readDouble();
+
   System.out.print("\nHow much does a case of soda cost: ");
   costOfSoda = IO.readDouble();
+
   System.out.print("\nHow many cans of soda are in each case: ");
   numSodaPerCase = IO.readDouble();
 
   pizzaToBuy = (int)((peopleAtParty * numSlicesPerPerson) / numSlicesPerPizza);
 
+  //check if the number of pies needed is fractional and if needed rounds up
   if (pizzaToBuy < ((peopleAtParty * numSlicesPerPerson) / numSlicesPerPizza)){
    pizzaToBuy += 1;
   }
 
+  //check if the number of cases needed is fractional and rounds up
   sodaToBuy = (int)((peopleAtParty * numSodasPerPerson) / numSodaPerCase);
 
   if (sodaToBuy < ((peopleAtParty * numSodasPerPerson) / numSodaPerCase)){
@@ -41,18 +50,9 @@ public class Party{
 
   totalCost = (pizzaToBuy * costOfPizza) + (sodaToBuy * costOfSoda);
 
-
-  // System.out.println(pizzaToBuy);
-  // System.out.println(pizzaToBuy * costOfPizza);
-
-  // System.out.println(sodaToBuy);
-  // System.out.println(sodaToBuy * costOfSoda);
-
-  // System.out.println(totalCost);
-  // System.out.println(0.62 + 0.97);
-
   System.out.println("");
 
+  //returns answer for rutgers webcat
   IO.outputDoubleAnswer(totalCost);
 
  }
