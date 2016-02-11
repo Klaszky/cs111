eb512b78685bf7e14ccf695b60755bda57c0389d
Caius <jfklaszky@gmail.com>
2016-02-11 00:35:04 -0500

    changed some var types to better fit

--- Party.java
+++ Party.java
@@ -5,8 +5,11 @@ public class Party{
   //welcome message
   System.out.println("java Party\n");
 
-  double peopleAtParty, numSlicesPerPerson, numSodasPerPerson;
-  double costOfPizza, numSlicesPerPizza, costOfSoda, numSodaPerCase;
+  //values to get from user
+  int peopleAtParty, numSlicesPerPerson, numSodasPerPerson, numSlicesPerPizza, numSodaPerCase;
+  
+  double costOfPizza, costOfSoda;
+
 
   int pizzaToBuy, sodaToBuy;
 
@@ -14,37 +17,37 @@ public class Party{
 
   //getting part info from user
   System.out.print("Enter number of people attending the party: ");
-  peopleAtParty = IO.readDouble();
+  peopleAtParty = IO.readInt();
 
   System.out.print("\nHow many slices of pizza will each person eat: ");
-  numSlicesPerPerson = IO.readDouble();
+  numSlicesPerPerson = IO.readInt();
 
   System.out.print("\nHow many cans of soda will each person drink: ");
-  numSodasPerPerson = IO.readDouble();
+  numSodasPerPerson = IO.readInt();
 
   System.out.print("\nHow much will each pizza pie cost: ");
   costOfPizza = IO.readDouble();
 
   System.out.print("\nHow many slices does each pie contain: ");
-  numSlicesPerPizza = IO.readDouble();
+  numSlicesPerPizza = IO.readInt();
 
   System.out.print("\nHow much does a case of soda cost: ");
   costOfSoda = IO.readDouble();
 
   System.out.print("\nHow many cans of soda are in each case: ");
-  numSodaPerCase = IO.readDouble();
+  numSodaPerCase = IO.readInt();
 
-  pizzaToBuy = (int)((peopleAtParty * numSlicesPerPerson) / numSlicesPerPizza);
+  pizzaToBuy = ((peopleAtParty * numSlicesPerPerson) / numSlicesPerPizza);
 
   //check if the number of pies needed is fractional and if needed rounds up
-  if (pizzaToBuy < ((peopleAtParty * numSlicesPerPerson) / numSlicesPerPizza)){
+  if (pizzaToBuy < ((peopleAtParty * numSlicesPerPerson) / (double)numSlicesPerPizza)){
    pizzaToBuy += 1;
   }
 
   //check if the number of cases needed is fractional and rounds up
-  sodaToBuy = (int)((peopleAtParty * numSodasPerPerson) / numSodaPerCase);
+  sodaToBuy = ((peopleAtParty * numSodasPerPerson) / numSodaPerCase);
 
-  if (sodaToBuy < ((peopleAtParty * numSodasPerPerson) / numSodaPerCase)){
+  if (sodaToBuy < ((peopleAtParty * numSodasPerPerson) / (double)numSodaPerCase)){
    sodaToBuy += 1;
   }
 
