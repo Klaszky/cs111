be9508b83c5f3edd5776607ec90e5fb285843cc3
Joseph <jfklaszky@gmail.com>
2016-03-30 17:35:53 -0400

    added array size test

--- MatrixOps.java
+++ MatrixOps.java
@@ -3,25 +3,53 @@ public class MatrixOps
 	public static void main(String[] args)
 	{
 		double[][] a = {
-			{1, 0, -2},
-			{0, 3, -1}
+			{0, 1, 2, 5, 3, 6},
+			{-2, 2, 3, 7,-2, 4},
+			{0, 3, 4, 42, 6, 4}
 		};
 
 		double[][] b = {
-			{0, 3},
-			{-2, -1},
-			{0, 4}
+			{1, 0, -2},
+			{0, 3, -1},
+			{3, 4, 6},
+			{1, 3, 4},
+			{5, 5, 6},
+			{0, 0, 0}
 		};
 
 		double[][] c = multiply(a, b);
 
-		More2D.prtArrD(c);
+		if(c == null)
+		{
+			System.out.println("null");
+		}
+		else
+		{
+			ArrayMethods.prtArrD(c);
+		}
 	}
 
 	public static double[][] multiply(double[][] A, double[][] B)
 	{
 		double[][] toReturn = new double[A.length][B[0].length];
 		double sum = 0;
+
+		for(int i = 0; i < A.length; i++)
+		{
+			if(A[i].length != B.length)
+			{
+				return null;
+			}
+		}
+
+		for(int i = 0; i < B.length; i++)
+		{
+			if(B[i].length != A.length)
+			{
+				return null;
+			}
+		}
+
 
 		for(int i = 0; i < A.length; i++)
 		{
